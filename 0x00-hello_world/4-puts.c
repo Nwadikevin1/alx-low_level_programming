c6c10a68f1e57d5d529da82bdaaa65e6f7cf78df
Nwadikevin1 <nwadikevin@gmail.com>
2022-09-09 14:40:43 -0700

    Puts added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Using the puts funtion to print out.
+ *
+ * Return: Always 0 (success).
+ */
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
