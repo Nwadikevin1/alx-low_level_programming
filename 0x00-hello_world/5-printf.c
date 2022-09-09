5ece29b3d221d28b8468c2b41dbcfb036e32c553
Nwadikevin1 <nwadikevin@gmail.com>
2022-09-09 15:12:15 -0700

    Added printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main -data entry
+ *
+ * Description: Using printf as an output.
+ *
+ * Return: Always 0  (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
